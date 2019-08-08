852db001ed4ee60a3f235f069032fe0d500548f8
isbaska <isbaska1@in.ibm.com>
2019-08-08 09:41:48 +0530

    scenario5 data binding

---
+++ src/app/component3/component3.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-component3',
+  templateUrl: './component3.component.html',
+  styleUrls: ['./component3.component.css']
+})
+export class Component3Component implements OnInit {
+
+  public message3 = '';
+  // Output directive
+  // instance of EventEmitter
+  @Output() public childEvent = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // emit message value as an event
+   fireEvent() {
+    this.childEvent.emit(this.message3);
+  }
+}
